b195c0bb57f0bdf1b1d056e6e0e3320bd3775389
tyler dobbs <tylerdobbs123@gmail.com>
2018-03-01 21:54:42 -0600

    Fix: api router

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import { join } from 'path';
 import stateRouting from './middleware/routing.mw';
+import api from './routers/api';
 
 const clientDist = join(__dirname, '..', '..', 'client', 'dist');
 
@@ -9,7 +10,8 @@ const configure = (app: express.Application): void => {
         .use(express.static(clientDist))
         .use(express.json())
         .use(express.urlencoded({ extended: false }))
-        .use(stateRouting);
+        .use(stateRouting)
+        .use('/api/v1', api);
 };
 
 export default configure;
