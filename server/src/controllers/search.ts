acba9b16193abe97a5fb797a13cf3993b083f162
tyler dobbs <tylerdobbs123@gmail.com>
2018-03-11 13:11:55 -0500

    Refactor: removed logs

--- server/src/controllers/search.ts
+++ server/src/controllers/search.ts
@@ -4,10 +4,7 @@ import procedures from '../procedures/search';
 function search(req: Request, res: Response, next: NextFunction) {
     res.promise = procedures.search(req.params.term)
         .then((results) => {
-            console.log('results', results);
             return results;
-        }, (err: Error) => {
-            console.error(err);
         });
     
     next();
