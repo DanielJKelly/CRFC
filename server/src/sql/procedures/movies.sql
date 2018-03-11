24de7004a16569eaa28cc02f63e248ed1f91058c
Daniel Kelly <djk8552@gmail.com>
2018-03-11 13:20:17 -0400

    Feat: Add InsertMovie stored proc

--- server/src/sql/procedures/movies.sql
+++ server/src/sql/procedures/movies.sql
@@ -21,3 +21,32 @@ BEGIN
 
 END$$
 DELIMITER ;
+
+DROP PROCEDURE IF EXISTS spInsertMovie;
+DELIMITER $$
+CREATE PROCEDURE spInsertMovie(
+    IN p_mdbid int,
+    IN p_title varchar(256),
+    IN p_director varchar(128),
+    IN p_poster varchar(256)
+)
+BEGIN
+
+    INSERT INTO Movies (
+        mdbid,
+        title,
+        director,
+        poster
+    )
+    VALUES (
+        p_mdbid,
+        p_title,
+        p_director,
+        p_poster
+    );
+
+    SELECT
+        LAST_INSERT_ID() AS id;
+
+END$$
+DELIMITER ;
