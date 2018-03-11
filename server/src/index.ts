50e86480f7b1b0c9920cb681152e22d03274c674
tyler dobbs <tylerdobbs123@gmail.com>
2018-03-11 13:14:03 -0500

    Feat: morgan logger

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,13 @@ import * as express from 'express';
 import { join } from 'path';
 import stateRouting from './middleware/routing.mw';
 import api from './routers/api';
+import * as morgan from 'morgan';
 
 const clientDist = join(__dirname, '..', '..', 'client', 'dist');
 
 const configure = (app: express.Application): void => {
     app
+        .use(morgan('dev'))
         .use(express.static(clientDist))
         .use(express.json())
         .use(express.urlencoded({ extended: false }))
