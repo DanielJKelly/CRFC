2e9270974d4c360e65ff47f0a2d6bcb486aa2d51
Daniel Kelly <djk8552@gmail.com>
2018-03-11 13:02:45 -0400

    Feat: Insert recommendation stored proc

---
+++ server/src/sql/procedures/recommendations.sql
@@ -0,0 +1,24 @@
+DROP PROCEDURE IF EXISTS spInsertRecommendation;
+DELIMITER $$
+CREATE PROCEDURE spInsertRecommendation(
+    IN p_recommenderid int,
+    IN p_recipientid int,
+    IN p_movieid int
+)
+BEGIN
+
+    INSERT INTO Recommendations (
+        recommenderid,
+        recipientid,
+        movieid,
+        isSeen
+    )
+    VALUES (
+        p_recommenderid,
+        p_recipientid,
+        p_movieid,
+        0
+    );
+
+END$$
+DELIMITER ;
