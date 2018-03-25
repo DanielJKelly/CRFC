c20d42ebfaf72817a1ad76b249bd1022ca3797fe
Daniel Kelly <djk8552@gmail.com>
2018-03-25 13:41:35 -0400

    Feat: Add stored proc for deleting a rating

--- server/src/sql/procedures/ratings.sql
+++ server/src/sql/procedures/ratings.sql
@@ -23,3 +23,19 @@ BEGIN
 
 END$$
 DELIMITER ;
+
+DROP PROCEDURE IF EXISTS spDeleteRating;
+DELIMITER $$
+CREATE PROCEDURE spDeleteRating(
+    IN p_id INT
+)
+BEGIN
+    DELETE 
+    FROM
+        Ratings
+    WHERE id = p_id
+    LIMIT 
+        1;   
+
+END$$
+DELIMITER ;
